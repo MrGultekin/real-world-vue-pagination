e040badae49df06639ebdb2ad607002a8ca48f51
MrGultekin <gultekin.de@gmail.com>
2022-10-20 23:28:19 +0200

    Programmatic Navigation with id but we don't need it, actually.

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,18 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
