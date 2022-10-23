40ac5c73e7fab9f7930c6dec68eecbad716394c0
MrGultekin <gultekin.de@gmail.com>
2022-10-23 21:16:38 +0200

    Flash message works.

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -9,12 +9,18 @@ export default {
   inject: ['GStore'],
   methods: {
     register() {
+      //Call to API
+      //Set a message to appear on the next page
+      this['GStore'].flashMessage =
+        'You have registered for ' + this.event.title
+
+      setTimeout(() => {
+        this['GStore'].flashMessage = ''
+      }, 3000)
+      //Why I cannot reach it like this.event
+
+      //If registered then redirect to event details
       this.$router.push({
-        //Call to API
-        //Set a message to appear on the next page
-
-        //If registered then redirect to event details
-
         name: 'EventDetails'
         // params: { id: this.event.id } not needed
       })
